8bb6b548ee99d63d9ad906007d1bcdae2b5954e9
Yohan Kim <ddalpange@gmail.com>
2023-04-23 15:21:22 +0900

    🛫

--- src/app/essay/page.tsx
+++ src/app/essay/page.tsx
@@ -1,11 +1,13 @@
 import { getAllPosts } from "~/post";
 import { PostView } from "~/app/components/PostView";
+import { Intro } from "../components/Intro";
 
 export default async function Essay() {
 	const posts = await getAllPosts();
 
 	return (
 		<main>
+			<Intro />
 			{posts
 				.filter((post) => post.tags.includes("ESSAY"))
 				.map((post) => {
