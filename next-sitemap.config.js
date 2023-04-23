c02ef2225a372dae62c4f245655840eb6b6899d9
Yohan Kim <ddalpange@gmail.com>
2023-04-23 15:38:49 +0900

    🛫

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
 	siteUrl: process.env.SITE_URL || "https://ddalpange.github.io",
-	generateRobotsTxt: true, // (optional)
+	generateRobotsTxt: true, // (optional),
+	outDir: "out",
 	// ...other options
 };
